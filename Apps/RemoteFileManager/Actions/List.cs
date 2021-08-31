b32facb070ef938c7e86b0f7b2b7478d060e6d34
Rolando Malena <rolando@opencare.com>
2021-08-30 23:12:19 -0400

    Added Regex supports to list action

--- Apps/RemoteFileManager/Actions/List.cs
+++ Apps/RemoteFileManager/Actions/List.cs
@@ -1,20 +1,26 @@
 ﻿using Google.Apis.Drive.v3;
 using RemoteFileManager.ExtensionMethods;
+using RemoteFileManager.Options;
 using System;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace RemoteFileManager.Actions
 {
     public static class List
     {
-        public static async Task Execute(DriveService service)
+        public static async Task Execute(DriveService service, ListOptions opts)
         {
             var fileRequest = service.Files.List();
             fileRequest.Fields = "*";
+            
             var files = (await fileRequest.ExecuteAsync()).Files;
-
-            foreach (var file in files.Where(f => f.MimeType != Constants.MimeTypes.Folder))
+            files = files
+                .Where(f => f.MimeType != Constants.MimeTypes.Folder && Regex.IsMatch(f.Name, opts.Regex))
+                .ToList();
+            
+            foreach (var file in files)
                 Console.WriteLine($"{file.Name}: {file.Size.Format()}.");
         }
     }
--- Apps/RemoteFileManager/Options/ListOptions.cs
+++ Apps/RemoteFileManager/Options/ListOptions.cs
@@ -6,11 +6,15 @@ namespace RemoteFileManager.Options
     [Verb("ls", HelpText = "List all files from Google Drive.")]
     public class ListOptions : BaseOptions
     {
+        [Value(index: 0, Required = false, HelpText = "Regex used to filter in files.", Default = "")]
+        public string Regex { get; set; }
+
         public override ActionFlow ActionFlow => ActionFlow.List;
 
         public override void PrintOptions()
         {
             Console.WriteLine($"Action: {ActionFlow}");
+            Console.WriteLine($"File Regex: {Regex}");
         }
     }
 }
